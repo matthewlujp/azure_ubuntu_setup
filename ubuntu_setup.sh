3e1493c3f9551d5fc113207792f7aa75e9efe831
Matthew Ishige <matthewlujp@gmail.com>
2017-02-24 13:48:37 +0900

    Add verbose

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -5,21 +5,24 @@
 # You should give your account name as the first params
 
 # Install packages
+echo "Installing packages ..."
 apt-get update
-apt-get install build-essential
-apt-get install git
-apt-get install libpng-dev
-apt-get install libbz2-dev
-apt-get install zlib1g-dev
-apt-get install libssl-dev
-apt-get install libreadline-dev
-apt-get install libsqlite3-dev
-apt-get install openssl
+apt-get install -y build-essential
+apt-get install -y git
+apt-get install -y libpng-dev
+apt-get install -y libbz2-dev
+apt-get install -y zlib1g-dev
+apt-get install -y libssl-dev
+apt-get install -y libreadline-dev
+apt-get install -y libsqlite3-dev
+apt-get install -y openssl
 
 # Generate SSH key
+echo "Generate SSH key ..."
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -P ""
 
 # Introduce pyenv
+echo "Installing pyenv and virtualenv..."
 sudo -u $1 cd /usr/local/
 git clone git://github.com/yyuu/pyenv.git ./pyenv
 mkdir -p ./pyenv/versions ./pyenv/shims
@@ -40,5 +43,6 @@ Defaults    env_keep += "PATH"
 Defaults    env_keep += "PYENV_ROOT"'
 
 # Install Python 3.6.0 with pyenv
+echo "Creating virtualenv with Python 3.6.0 ..."
 prenv install 3.6.0
 pyenv virtualenv 3.6.0 'machine_learning'
